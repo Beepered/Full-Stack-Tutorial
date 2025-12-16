ddbc3fe1a614c13837249a04d10b954269c5a2af
Beepered <bktrieu@gmail.com>
2025-12-15 16:44:28 -0800

    got render site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("/contacts");  // http://127.0.0.1:5000/contacts
+    const response = await fetch("https://full-stack-tutorial-backend.onrender.com//contacts");  // http://127.0.0.1:5000/contacts
     const data = await response.json();
     setContacts(data.contacts);
   };
